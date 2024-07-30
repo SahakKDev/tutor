c027062761376af90d1d74d58641d0023bcc1f77
SahakK <sahak.keshishyan.dev@gmail.com>
2024-07-30 15:52:53 +0400

    hero section done, change navbar bg color on scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Toggle mobile menu */
 document.addEventListener("DOMContentLoaded", () => {
   const burgerBtn = document.querySelector(".navbar .mobile-menu-toggle");
   const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
@@ -5,4 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
   burgerBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
   });
+});
+
+/* Change navbar bg on scroll */
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY === 0) {
+    navbar.classList.remove("navbar-scroll");
+  } else {
+    navbar.classList.add("navbar-scroll");
+  }
 });
